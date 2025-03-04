cb7518dbdec2adb440e1daf4321da99f618d49cc
surubasnet <surubasnet@gmail.com>
2025-03-04 09:01:19 +0545

    Create completed

--- base/views.py
+++ base/views.py
@@ -8,5 +8,13 @@ def home(request):
     return render(request,'index.html',context={'notes':note_objs})
 
 def create_note(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        file = request.POST.get('file')
+        type = request.POST.get('type')
+        deadline_at = request.POST.get('deadline_at')
+        note_type_obj = NoteType.objects.get(id=type)
+        Note.objects.create(name=name,description=description,file=file,type=note_type_obj,deadline_at=deadline_at)
     note_type_objs = NoteType.objects.all()
     return render(request,'create_note.html',context={'note_types':note_type_objs})
